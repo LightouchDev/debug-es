28bdc0d38db1127afa90f3c5c07f6c25fec155b7
Miau Lightouch <miaulightouch@gmail.com>
2018-05-17 18:57:11 +0800

    Refactoring codebase
    close visionmedia/debug#569
    close visionmedia/debug#563

---
+++ test/browser.test.js
@@ -0,0 +1,245 @@
+/**
+ * Test case for browser environment
+ */
+const env = 'browser'
+const process = global.process
+
+process.browser = true
+
+const localStorage = window.localStorage = window.sessionStorage = {
+  getItem (key) {
+    const value = this[key]
+    return typeof value === 'undefined'
+      ? null
+      : value
+  },
+  setItem (key, value) {
+    this[key] = value
+  },
+  removeItem (key) {
+    return delete this[key]
+  }
+}
+
+const commonTest = require('./common')
+const { setWildcard } = commonTest
+commonTest(env, tests)
+
+function tests () {
+  beforeEach(() => { window.localStorage = localStorage })
+
+  describe('detect environment', () => {
+    beforeEach(() => {
+      global.process = process
+      Object.assign(process, {
+        type: undefined,
+        __nwjs: undefined,
+        browser: undefined
+      })
+      window.localStorage.removeItem('debug')
+    })
+
+    afterAll(() => { process.browser = true })
+
+    test(`browser: no global process object`, () => {
+      global.process = undefined
+
+      const debug = require('../src')
+      global.process = process
+      expect(debug).toHaveProperty('storage', expect.anything())
+    })
+    test(`Electron: process.type === 'renderer'`, () => {
+      process.type = 'renderer'
+
+      const debug = require('../src')
+      expect(debug).toHaveProperty('storage', expect.anything())
+    })
+    test(`nwjs: process.__nwjs is truthy`, () => {
+      process.__nwjs = 1
+
+      const debug = require('../src')
+      expect(debug).toHaveProperty('storage', expect.anything())
+    })
+    test(`'process' package support`, () => {
+      process.browser = true
+
+      const debug = require('../src')
+      expect(debug).toHaveProperty('storage', expect.anything())
+    })
+  })
+
+  describe('formatters', () => {
+    describe('%j formatter', () => {
+      const debug = require('../src')
+      const { j: formatter } = debug.formatters
+
+      // this test borrow from fast-json-stringify
+      test('common object', () => {
+        const obj = {
+          stringProperty: 'string1',
+          objectProperty: {
+            stringProperty: 'string2',
+            numberProperty: 42
+          }
+        }
+        expect(formatter(obj))
+          .toBe('{"stringProperty":"string1","objectProperty":{"stringProperty":"string2","numberProperty":42}}')
+      })
+
+      test('circular object', () => {
+        const obj = {}
+        obj.this = obj
+
+        expect(formatter(obj)).toBe('{}')
+      })
+
+      test.skip('error handle', () => {
+        // FIXME: it's hard to make stringify throw error
+        const error = new Error('this is a fake error')
+        const obj = {}
+        Object.defineProperty(obj, 'error', {
+          get () { throw error }
+        })
+
+        expect(formatter(obj)).toBe(`[UnexpectedJSONParseError]: ${error.message}`)
+      })
+    })
+  })
+
+  describe('storage', () => {
+    test('localStorage exist', () => {
+      const debug = require('../src')
+      expect(debug.storage).toBe(localStorage)
+    })
+
+    test('localStorage not exist', () => {
+      delete window.localStorage
+      const debug = require('../src')
+      window.localStorage = localStorage
+      expect(debug.storage).toBeUndefined()
+    })
+  })
+
+  describe('formatArgs', () => {
+    beforeEach(() => setWildcard(env))
+
+    test('color output: on', () => {
+      const debug = require('../src')
+      const info = debug('info')
+      info.log = jest.fn()
+      info('%%this is message%%')
+      expect(info.log).toBeCalledWith('%cinfo %c%%this is message%% %c+0ms', 'color: #CC3300', 'color: inherit', 'color: #CC3300')
+    })
+
+    test('color output: off', () => {
+      const debug = require('../src')
+      const info = debug('info')
+      info.log = jest.fn()
+      info.useColors = false
+      info('%%this is message%%')
+      expect(info.log).toBeCalledWith('info %%this is message%% +0ms')
+    })
+
+    test('custom color tag', () => {
+      const debug = require('../src')
+      const info = debug('info')
+      info.log = jest.fn()
+      info('%%this is %cmessage%%', 'color: #ff0000')
+      expect(info.log).toBeCalledWith('%cinfo %c%%this is %cmessage%% %c+0ms', 'color: #CC3300', 'color: inherit', 'color: #ff0000', 'color: #CC3300')
+    })
+  })
+
+  describe('load', () => {
+    test('load from localStorage', () => {
+      const namespaces = 'test, -dummy, worker:*'
+      window.localStorage.setItem('debug', namespaces)
+
+      const browser = require('../src/browser')
+      expect(browser.load()).toBe(namespaces)
+    })
+
+    test('load from process.env', () => {
+      const namespaces = 'test, -dummy, worker:*'
+      process.env.DEBUG = namespaces
+      expect(window.localStorage.getItem('debug')).toBeFalsy()
+
+      const browser = require('../src/browser')
+      expect(browser.load()).toBe(namespaces)
+    })
+  })
+
+  describe('save', () => {
+    const namespaces = 'test, -dummy, worker:*'
+
+    test('null namespaces', () => {
+      const browser = require('../src/browser')
+      window.localStorage.setItem('debug', namespaces)
+      browser.save()
+
+      expect(browser.load()).toBeUndefined()
+    })
+
+    test('valid namespaces', () => {
+      const browser = require('../src/browser')
+      browser.save(namespaces)
+
+      expect(browser.load()).toBe(namespaces)
+    })
+  })
+
+  describe('useColors', () => {
+    const ie11 = 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko'
+    const edge = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
+    const edgeOld = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136'
+    const firefox = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'
+    const chrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36'
+
+    Object.defineProperty(navigator, 'userAgent', (_value => {
+      return {
+        get () {
+          return _value
+        },
+        set (value) {
+          _value = value
+        }
+      }
+    })(navigator.userAgent))
+
+    test('Electron/nwjs', () => {
+      const debug = require('../src')
+      process.type = 'renderer'
+      expect(debug.useColors()).toBe(true)
+      delete process.type
+
+      process.__nwjs = 1
+      expect(debug.useColors()).toBe(true)
+      delete process.__nwjs
+    })
+
+    test('IE 11', () => {
+      navigator.userAgent = ie11
+
+      expect(require('../src').useColors()).toBe(false)
+    })
+    test('edge 12.10136', () => {
+      navigator.userAgent = edgeOld
+
+      expect(require('../src').useColors()).toBe(false)
+    })
+    test('edge 16.16299', () => {
+      navigator.userAgent = edge
+
+      expect(require('../src').useColors()).toBe(true)
+    })
+    test('firefox 61', () => {
+      navigator.userAgent = firefox
+
+      expect(require('../src').useColors()).toBe(true)
+    })
+    test('chrome 66', () => {
+      navigator.userAgent = chrome
+
+      expect(require('../src').useColors()).toBe(true)
+    })
+  })
+}
---
+++ test/common.js
@@ -0,0 +1,232 @@
+/* eslint-disable no-console */
+
+function resetEnv (env) {
+  jest.resetModules()
+
+  if (env === 'browser') {
+    window.localStorage.removeItem('debug')
+  }
+
+  if (env === 'node') {
+    Object.keys(process.env)
+      .filter(key => /^debug_/i.test(key))
+      .forEach(key => delete process.env[key])
+  }
+  delete process.env.DEBUG
+}
+
+function setWildcard (env) {
+  env === 'browser' && window.localStorage.setItem('debug', '*')
+  env === 'node' && (process.env.DEBUG = '*')
+}
+
+module.exports = (env, tests) => {
+  beforeEach(() => resetEnv(env))
+
+  describe(`common test in ${env}`, () => {
+    describe('common.js', () => {
+      describe('coerce: handle Error messages', () => {
+        const debug = require('../src')
+        const message = 'this is message'
+
+        test('Error object', () => {
+          const err = new Error(message)
+          expect(debug.coerce(err)).toBe(err.stack)
+        })
+        test('Error object without .stack', () => {
+          const err = new Error(message)
+          delete err.stack
+          expect(debug.coerce(err)).toBe(err.message)
+        })
+        test('plain strings', () => {
+          expect(debug.coerce(message)).toBe(message)
+        })
+      })
+
+      test('disable: turn off all instances', () => {
+        const debug = require('../src')
+        debug.disable()
+        expect(debug.skips).toHaveLength(0)
+        expect(debug.names).toHaveLength(0)
+
+        expect(debug.instances.some(instance => instance.enabled)).toBeFalsy()
+      })
+
+      describe('enable', () => {
+        function instancesCalc (debug) {
+          let truthy = 0
+          let falsy = 0
+          debug.instances.forEach(instance => instance.enabled ? truthy++ : falsy++)
+          return { falsy, truthy }
+        }
+
+        test('turn on instances by namespaces', () => {
+          const debug = require('../src')
+          debug('test')
+          debug('dummy')
+          debug('worker:a')
+          debug('worker:b')
+
+          const namespaces = 'test, -dummy, worker:*'
+          debug.enable(namespaces)
+
+          expect(debug.load()).toBe(namespaces)
+
+          expect(debug.names).toHaveLength(2)
+          expect(debug.names.map(regex => regex.toString())).toEqual(['/^test$/', '/^worker:.*?$/'])
+
+          expect(debug.skips).toHaveLength(1)
+          expect(debug.skips.map(regex => regex.toString())).toEqual(['/^dummy$/'])
+
+          expect(instancesCalc(debug)).toEqual({ falsy: 1, truthy: 3 })
+        })
+
+        test('wildcard with some skip names', () => {
+          const debug = require('../src')
+          debug('test')
+          debug('dummy')
+          debug('worker:a')
+          debug('worker:b')
+          debug('nickname')
+          debug.enable('*, -dummy, -*name')
+
+          expect(debug.names).toHaveLength(1)
+          expect(debug.skips).toHaveLength(2)
+          expect(instancesCalc(debug)).toEqual({ falsy: 2, truthy: 3 })
+        })
+
+        test('non-string namespaces', () => {
+          const debug = require('../src')
+          expect(() => debug.enable(true)).not.toThrow()
+        })
+      })
+
+      test('enabler: check namespace enabled', () => {
+        const debug = require('../src')
+
+        expect(debug.enabler('')).toBe(false)
+        expect(debug.enabler('name*')).toBe(true)
+
+        debug.enable('test, -dummy, worker:*')
+        expect(debug.enabler('test')).toBe(true)
+        expect(debug.enabler('test123')).toBe(false)
+        expect(debug.enabler('dummy')).toBe(false)
+        expect(debug.enabler('worker:a')).toBe(true)
+        expect(debug.enabler('worker:b')).toBe(true)
+        expect(debug.enabler('work:a')).toBe(false)
+      })
+    })
+
+    describe('debug.js', () => {
+      beforeEach(() => setWildcard(env))
+
+      test('debug is disabled', () => {
+        resetEnv(env)
+        const debug = require('../src')
+        debug.log = jest.fn()
+        const info = debug('info')
+        info('this is disabled')
+
+        expect(debug.log).not.toBeCalled()
+      })
+
+      describe('formatter', () => {
+        test('% escape', () => {
+          const debug = require('../src')
+          debug.formatArgs = jest.fn()
+          debug.log = () => {}
+
+          const message = '%% message %%'
+          const info = debug('info')
+          info(message)
+
+          expect(debug.formatArgs).toBeCalledWith([message])
+        })
+
+        test('custom formatter', () => {
+          const debug = require('../src')
+          debug.formatArgs = jest.fn()
+          debug.log = () => {}
+          debug.formatters.z = value => value.toUpperCase()
+
+          const info = debug('info')
+          info('%% %z %%', 'message')
+
+          expect(debug.formatArgs).toBeCalledWith(['%% MESSAGE %%'])
+        })
+      })
+
+      describe('log function', () => {
+        test('default log()', () => {
+          const debug = require('../src')
+          debug.log = jest.fn()
+
+          const info = debug('test')
+          info('this is a message')
+
+          expect(info.log).toBeFalsy()
+          expect(debug.log).toBeCalled()
+        })
+
+        test('custom log()', () => {
+          const debug = require('../src')
+
+          const info = debug('test')
+          info.log = jest.fn()
+          info('this is a message')
+
+          expect(info.log).toBeCalled()
+        })
+      })
+
+      describe('destroy', () => {
+        test('destroy instance', () => {
+          const debug = require('../src')
+          debug('test')
+          const info = debug('info')
+
+          expect(debug.instances).toHaveLength(2)
+          expect(debug.instances.indexOf(info)).toBeGreaterThan(-1)
+
+          expect(info.destroy()).toBe(true)
+          expect(debug.instances).toHaveLength(1)
+          expect(debug.instances.indexOf(info)).toBe(-1)
+        })
+
+        test('destroy instance that not in instances array', () => {
+          const debug = require('../src')
+          const info = debug('info')
+          info.enabled = false
+
+          debug.enable('*')
+          expect(info.enabled).toBe(true)
+
+          // clear array
+          debug.instances.length = 0
+          expect(debug.instances.indexOf(info)).toBe(-1)
+
+          info.enabled = false
+          expect(info.destroy()).toBe(false)
+
+          debug.enable('*')
+          expect(info.enabled).toBe(false)
+        })
+      })
+    })
+
+    test('basic sanity check', () => {
+      const debug = require('../src')
+      const info = debug('info')
+      info.enabled = true
+      expect(() => info('hello world')).not.toThrow()
+      expect(() => info({})).not.toThrow()
+    })
+  })
+
+  tests && describe(`${env}-specific tests`, tests)
+}
+
+Object.assign(module.exports, {
+  resetEnv,
+  setWildcard
+})
